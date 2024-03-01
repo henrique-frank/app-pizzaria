b0e7633507bceca489172f0d3198b646d6570b83
henrique-frank <henrique_frank@hotmail.com>
2024-03-01 13:18:32 -0300

    Backend inital configuration

---
+++ backend/src/routes.ts
@@ -0,0 +1,10 @@
+import { Router, Request, Response} from 'express';
+
+const router = Router();
+
+router.get('/teste', (req: Request, res: Response) => {
+    return res.json({ nome: "App Pizza"})
+    // throw new Error ('Erro ao fazer essa requisicao');
+})
+
+export { router };
