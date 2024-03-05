cf9c1e0c05349b298274ef079ae6c23c265fe0bb
henrique-frank <henrique_frank@hotmail.com>
2024-03-05 08:57:23 -0300

    Adding CreateUserService

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,8 @@
-import { Router, Request, Response} from 'express';
+import { Router } from 'express';
+import { CreateUserController } from './controllers/user/CreateUserController';
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ nome: "App Pizza"})
-    // throw new Error ('Erro ao fazer essa requisicao');
-})
+router.post('/users', new CreateUserController().handle)
 
 export { router };
