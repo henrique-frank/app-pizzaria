28ca00a0bb814b6426445b14806c6e5d0a4bad65
henrique-frank <henrique_frank@hotmail.com>
2024-03-06 10:55:33 -0300

    Adding JWT Token for login

--- backend/src/services/user/AuthUserService.ts
+++ backend/src/services/user/AuthUserService.ts
@@ -1,6 +1,7 @@
 
 import prismaClient from "../../prisma"
 import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
 
 interface AuthUserRequest{
     email: string;
@@ -29,7 +30,29 @@ class AuthUserService{
             throw new Error("User/password incorrect")
         }        
 
-        return { ok: true}
+        // gerar token JWT com dados como id, nome e email
+
+            const token = sign(
+                {
+                    name: user.name,
+                    email: user.email
+                },
+                process.env.JWT_SECRET,
+                {
+                    subject: user.id,
+                    expiresIn: '30d'
+                }
+            )
+
+
+
+        return { 
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+
+        }
     }
 }
 
